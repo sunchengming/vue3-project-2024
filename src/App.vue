6486e6879574837bf71c1b994f5024b57bd05df9
sunchengming <sunchengming@everelegance.cn>
2024-10-30 05:38:58 +0800

    提交login部分代码

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,7 @@
-<script setup>
-// import { useRoute, useRouter } from 'vue-router';
-import { userUserStore, userCountStore } from '@/stores/index';
-// const router = useRouter();
-// const route = useRoute();
-
-const userStore = userUserStore();
-const countStore = userCountStore();
-</script>
+<script setup></script>
 <template>
     <div>
         <router-view></router-view>
-        <test-demo></test-demo>
-        我是app
-        <p>{{ userStore.token }}</p>
-        <el-button @click="userStore.setToken('1234564')">登录</el-button>
-        <el-button @click="userStore.removeToken()">退出</el-button>
-        <hr />
-        {{ countStore.count }}
-        <el-button @click="countStore.addCount(22)">添加</el-button>
     </div>
 </template>
 
--- src/views/login/LoginPage.vue
+++ src/views/login/LoginPage.vue
@@ -1,3 +1,34 @@
+<script setup></script>
 <template>
-    <div class="login">登录页面</div>
+    <el-row class="login-page">
+        <el-col :span="12" class="bg"></el-col>
+        <el-col :span="6" :offset="3" class="form">
+            <el-form>
+                <el-form-item>
+                    <h1>注册</h1>
+                </el-form-item>
+            </el-form>
+        </el-col>
+    </el-row>
 </template>
+
+<style lang="scss" scoped>
+.login-page {
+    height: 100vh;
+    background-color: #fff;
+    .bg {
+        background:
+            url('@/assets/logo2.png') no-repeat 60% center / 240px auto,
+            url('@/assets/login_bg.jpg') no-repeat center / cover;
+        border-radius: 0 20px 20px 0;
+    }
+    .form {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        .title {
+            margin: 0 auto;
+        }
+    }
+}
+</style>
